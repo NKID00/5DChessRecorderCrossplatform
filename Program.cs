1cb8e3e62e188cdbdc7105e196a566fb58779228
NKID00 <nkid00@pm.me>
2022-06-27 00:53:38 +0800

    refactor: port more

--- Program.cs
+++ Program.cs
@@ -27,6 +27,52 @@ namespace FiveDChessRecorderCrossplatform
             RecordGames(di);
         }
 
+        private static string GenerateFEN(ChessBoard board, string timeline, string turn)
+        {
+            var pieces = "";
+            for (int y = board.height - 1; y >= 0; y--)
+            {
+                if (y < board.height - 1)
+                {
+                    pieces += "/";
+                }
+                for (int x = 0; x < board.width; x++)
+                {
+                    var p = board.Pieces[x * board.height + y];
+                    if (p.IsEmpty)
+                    {
+                        var i = 0;
+                        while (x < board.width && board.Pieces[x * board.height + y].IsEmpty)
+                        {
+                            i += 1;
+                            x += 1;
+                        }
+                        pieces += $"{i}";
+                    }
+                    else
+                    {
+                        pieces += GenerateFENSymbol(p);
+                    }
+                }
+            }
+            return $"\r\n[{pieces}:{timeline}:{turn}:{(board.cbm.isBlacksMove == 0 ? 'w' : 'b')}]";
+        }
+
+        private static string GenerateFENSymbol(ChessBoard.ChessPiece piece)
+        {
+            var p = piece.Kind switch
+            {
+                ChessBoard.ChessPiece.PieceKind.Pawn => "P*",
+                ChessBoard.ChessPiece.PieceKind.King => "K*",
+                ChessBoard.ChessPiece.PieceKind.Rook => "R*",
+                ChessBoard.ChessPiece.PieceKind.Brawn => "W*",
+                _ => piece.SingleLetterNotation()
+            };
+
+            if (piece.IsBlack) return p.ToLower();
+            else return p;
+        }
+
         private static void RecordGames(DataInterface di)
         {
             var chessboardLocation = di.MemLocChessArrayPointer.GetValue();
@@ -44,21 +90,27 @@ namespace FiveDChessRecorderCrossplatform
             var startDate = "";
             var startTime = "";
             var localPlayer = -1;
+            var memLocEvenTimelines = new FiveDChessDataInterface.MemoryHelpers.MemoryLocation<int>(di.GetGameHandle(), di.MemLocChessArrayPointer.Location, -0x34); // 0 if odd number of starting timelines, -1 if even
+            var memLocWhichPlayerIsLocal = new FiveDChessDataInterface.MemoryHelpers.MemoryLocation<int>(di.GetGameHandle(), di.MemLocChessArrayPointer.Location, -0x44);
             while (true)
             {
                 var thisP = di.GetCurrentPlayersTurn();
                 var thisTime = di.GetCurT();
-                if(thisP!=lastP){// Reset timers
-                    lastTime=thisTime;
-                    lastP=thisP;
+                if (thisP != lastP)
+                {// Reset timers
+                    lastTime = thisTime;
+                    lastP = thisP;
                 }
-                if (lastTime!=0 && (thisTime-1)*3<=(lastTime-1)*2){//Should always tick if thisTime==1 or thisTime==0
-                    try{
+                if (lastTime != 0 && (thisTime - 1) * 3 <= (lastTime - 1) * 2)
+                {//Should always tick if thisTime==1 or thisTime==0
+                    try
+                    {
                         // player.Play();
                     }
-                    catch {
+                    catch
+                    {
                     }
-                    lastTime=thisTime;
+                    lastTime = thisTime;
                     //System.Media.SystemSounds.Exclamation.Play();
                 }
                 // Potential reasons to beep:
@@ -67,7 +119,7 @@ namespace FiveDChessRecorderCrossplatform
                 // half the time you started the turn with
                 // When you use up your increment
                 // when you're significantly behind your opponent
-                
+
                 var cnt = di.GetChessBoardAmount();
                 var turn = di.GetCurrentPlayersTurn();
                 //turn != oldTurn is there to make sure we record times, but we shouldn't rely 
@@ -77,39 +129,44 @@ namespace FiveDChessRecorderCrossplatform
                 // there's only a problem if that all happens in half a second, so I'm not too worried, but if we find the memory location of the move number,
                 //that would be better than tracking the turn.
                 var state = di.GetCurrentGameState();
-                if (cnt != oldCnt || turn != oldTurn || state!=oldState)
+                if (cnt != oldCnt || turn != oldTurn || state != oldState)
                 {
-                    var cosmeticTurnOffset = 1+di.MemLocCosmeticTurnOffset.GetValue();
-                    var evenTLs = di.MemLocEvenTimelines.GetValue();
-                    Func<int,string> mkL = (l)=>{
-                        if (evenTLs==0)return $"{l}";
-                        else if(l>=0) return $"+{l}";
-                        else if(l==-1) return "-0";
-                        else return $"{l+1}";
-                        };
-                    Func<int,string> mkT = (t)=>{return $"{t+cosmeticTurnOffset}";};
-                    Func<int,int,string> mkLT = (l,t)=>{return $"({mkL(l)}T{mkT(t)})";};
-                    if (cnt==0 && oldCnt>5 && !written){
-                        var filename = "5dpgn"+DateTime.Now.ToString("yyyyMMdd_HHmmss")+".txt";
-                        File.WriteAllText(filename,lastRun);
+                    var cosmeticTurnOffset = 1 + di.MemLocCosmeticTurnOffset.GetValue();
+                    var evenTLs = memLocEvenTimelines.GetValue();
+                    Func<int, string> mkL = (l) =>
+                    {
+                        if (evenTLs == 0) return $"{l}";
+                        else if (l >= 0) return $"+{l}";
+                        else if (l == -1) return "-0";
+                        else return $"{l + 1}";
+                    };
+                    Func<int, string> mkT = (t) => { return $"{t + cosmeticTurnOffset}"; };
+                    Func<int, int, string> mkLT = (l, t) => { return $"({mkL(l)}T{mkT(t)})"; };
+                    if (cnt == 0 && oldCnt > 5 && !written)
+                    {
+                        var filename = "5dpgn" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".txt";
+                        File.WriteAllText(filename, lastRun);
                         Console.WriteLine($"written to file '{filename}'");
                     }
-                    if (oldCnt==0){
+                    if (oldCnt == 0)
+                    {
                         times = new List<int>();
                         written = false;
                         startDate = DateTime.Now.ToString("yyyy.MM.dd");
                         startTime = DateTime.Now.ToString("HH:mm:ss (zzz)");
-                        localPlayer = di.whichPlayerIsLocal(); //0 white, 1 black, 2 both
+                        localPlayer = memLocWhichPlayerIsLocal.GetValue(); //0 white, 1 black, 2 both
                     }
-                    fen="\r\n[Board \"custom\"]";
-                    lastRun=$"[Mode \"5D\"]\r\n[Result \"{GameStateToResult(state)}\"]\r\n[Date \"{startDate}\"]\r\n[Time \"{startTime}\"]\r\n[Size \"{di.GetChessBoardSize().toString()}\"]";
-                    if(localPlayer==1){
-                        lastRun+="\r\n[White \"Opponent\"]";
+                    fen = "\r\n[Board \"custom\"]";
+                    lastRun = $"[Mode \"5D\"]\r\n[Result \"{GameStateToResult(state)}\"]\r\n[Date \"{startDate}\"]\r\n[Time \"{startTime}\"]\r\n[Size \"{di.GetChessBoardSize().Width}x{di.GetChessBoardSize().Height}\"]";
+                    if (localPlayer == 1)
+                    {
+                        lastRun += "\r\n[White \"Opponent\"]";
                         //lastRun+="\r\n[Black \"tesseract\"]";
                     }
-                    if(localPlayer==0){
+                    if (localPlayer == 0)
+                    {
                         //lastRun+="\r\n[White \"tesseract\"]";
-                        lastRun+="\r\n[Black \"Opponent\"]";
+                        lastRun += "\r\n[Black \"Opponent\"]";
                     }
                     oldCnt = cnt;
                     oldTurn = turn;
@@ -122,81 +179,97 @@ namespace FiveDChessRecorderCrossplatform
                     {
                         var board1 = cbs[i];
                         var mem1 = board1.cbm;
-                        if (mem1.boardId!=i) Console.WriteLine("Warning: id does not match index");
+                        if (mem1.boardId != i) Console.WriteLine("Warning: id does not match index");
                         //fen+=$"\r\ni:{i}; mn:{mem1.moveNumber}; pb:{mem1.previousBoardId}; msx:{mem1.moveSourceX}; cmn:{mem1.creatingMoveNumber}";
-                        if (mem1.previousBoardId==-1){
+                        if (mem1.previousBoardId == -1)
+                        {
                             // Add to FEN
-                            fen+=board1.toFEN(mkL(mem1.timeline),mkT(mem1.turn));
+                            fen += GenerateFEN(board1, mkL(mem1.timeline), mkT(mem1.turn));
                             continue;
                         }
                         var board = cbs[mem1.previousBoardId];
                         var mem = board.cbm;
-                        if (mem.moveSourceL<=0 && mem.moveSourceT<=0 && mem.moveSourceX<=0 && mem.moveSourceY<=0
-                                && mem.moveDestL<=0 && mem.moveDestT<=0 && mem.moveDestX<=0 && mem.moveDestY<=0
-                                && (mem1.creatingMoveNumber==mem.moveNumber || mem.moveNumber==-1)){
-                            fen+=board1.toFEN(mkL(mem1.timeline),mkT(mem1.turn));
+                        if (mem.moveSourceL <= 0 && mem.moveSourceT <= 0 && mem.moveSourceX <= 0 && mem.moveSourceY <= 0
+                                && mem.moveDestL <= 0 && mem.moveDestT <= 0 && mem.moveDestX <= 0 && mem.moveDestY <= 0
+                                && (mem1.creatingMoveNumber == mem.moveTurn || mem.moveTurn == -1))
+                        {
+                            fen += GenerateFEN(board1, mkL(mem1.timeline), mkT(mem1.turn));
                             continue;
                         }
-                          
-                        
-                        if (mem.isBlacksMove!=lastCol){
+
+
+                        if (mem.isBlacksMove != lastCol)
+                        {
                             lastCol = mem.isBlacksMove;
                             char c = 'w';
-                            if (lastCol==1){
+                            if (lastCol == 1)
+                            {
                                 c = 'b';
-                                lastRun+=$"/ ";
+                                lastRun += $"/ ";
                             }
-                            else{
-                                turnNumber+=1;
-                                if(turnNumber==1) lastRun+=fen;
-                                lastRun+=$"\r\n{turnNumber}.";
+                            else
+                            {
+                                turnNumber += 1;
+                                if (turnNumber == 1) lastRun += fen;
+                                lastRun += $"\r\n{turnNumber}.";
                             }
-                            var n = turnNumber*2 + lastCol - 1;
-                            if ( times.Count < n){
-                                times.Add(c=='w'?di.GetWT():di.GetBT());
+                            var n = turnNumber * 2 + lastCol - 1;
+                            if (times.Count < n)
+                            {
+                                times.Add(c == 'w' ? di.GetWT() : di.GetBT());
                             }
-                            else if(times.Count == n){
-                                times[n-1] = c=='w'?di.GetWT():di.GetBT();
+                            else if (times.Count == n)
+                            {
+                                times[n - 1] = c == 'w' ? di.GetWT() : di.GetBT();
                             }
-                            if(times[0]!=0){
-                                var t = times[n-1];
-                                lastRun+=$"{{{t/60}:{t%60:00}}}";
+                            if (times[0] != 0)
+                            {
+                                var t = times[n - 1];
+                                lastRun += $"{{{t / 60}:{t % 60:00}}}";
                             }
                         }
-                        var movetype=0;//Physical
+                        var movetype = 0;//Physical
                         //work out if it's a branch or a hop
-                        if (i+1<cbs.Count){
-                            if (cbs[i+1].cbm.creatingMoveNumber==mem1.creatingMoveNumber){
-                                if (cbs[i+1].cbm.timeline==mem.moveDestL){
-                                    movetype=1; //Hop
+                        if (i + 1 < cbs.Count)
+                        {
+                            if (cbs[i + 1].cbm.creatingMoveNumber == mem1.creatingMoveNumber)
+                            {
+                                if (cbs[i + 1].cbm.timeline == mem.moveDestL)
+                                {
+                                    movetype = 1; //Hop
                                 }
-                                else{
-                                    movetype=2; //Branch
+                                else
+                                {
+                                    movetype = 2; //Branch
                                 }
-                                i+=1; // Skip the second board created by the move
+                                i += 1; // Skip the second board created by the move
                             }
                         }
-                        if (movetype==0){
-                            lastRun+=($"{mkLT(mem.moveSourceL,mem.moveSourceT)}{board.Pieces[mem.moveSourceX*board.height+mem.moveSourceY].Notation()}{(char)(97+mem.moveSourceX)}{1+mem.moveSourceY}{(char)(97+mem.moveDestX)}{1+mem.moveDestY} ");
+                        if (movetype == 0)
+                        {
+                            lastRun += ($"{mkLT(mem.moveSourceL, mem.moveSourceT)}{board.Pieces[mem.moveSourceX * board.height + mem.moveSourceY].SingleLetterNotation()}{(char)(97 + mem.moveSourceX)}{1 + mem.moveSourceY}{(char)(97 + mem.moveDestX)}{1 + mem.moveDestY} ");
                         }
-                        else if (movetype==1){
-                            lastRun+=($"{mkLT(mem.moveSourceL,mem.moveSourceT)}{board.Pieces[mem.moveSourceX*board.height+mem.moveSourceY].Notation()}{(char)(97+mem.moveSourceX)}{1+mem.moveSourceY}>{mkLT(mem.moveDestL,mem.moveDestT)}{(char)(97+mem.moveDestX)}{1+mem.moveDestY} ");
+                        else if (movetype == 1)
+                        {
+                            lastRun += ($"{mkLT(mem.moveSourceL, mem.moveSourceT)}{board.Pieces[mem.moveSourceX * board.height + mem.moveSourceY].SingleLetterNotation()}{(char)(97 + mem.moveSourceX)}{1 + mem.moveSourceY}>{mkLT(mem.moveDestL, mem.moveDestT)}{(char)(97 + mem.moveDestX)}{1 + mem.moveDestY} ");
                         }
-                        else{ //movetype==2
-                            lastRun+=($"{mkLT(mem.moveSourceL,mem.moveSourceT)}{board.Pieces[mem.moveSourceX*board.height+mem.moveSourceY].Notation()}{(char)(97+mem.moveSourceX)}{1+mem.moveSourceY}>>{mkLT(mem.moveDestL,mem.moveDestT)}{(char)(97+mem.moveDestX)}{1+mem.moveDestY} ");
+                        else
+                        { //movetype==2
+                            lastRun += ($"{mkLT(mem.moveSourceL, mem.moveSourceT)}{board.Pieces[mem.moveSourceX * board.height + mem.moveSourceY].SingleLetterNotation()}{(char)(97 + mem.moveSourceX)}{1 + mem.moveSourceY}>>{mkLT(mem.moveDestL, mem.moveDestT)}{(char)(97 + mem.moveDestX)}{1 + mem.moveDestY} ");
                         }
                     }
-                    if(turnNumber==0)lastRun+=fen;
+                    if (turnNumber == 0) lastRun += fen;
                     //Console.WriteLine(fen);
                     Console.WriteLine(lastRun);
-                    if (( state==GameState.EndedBlackWon
-                          || state==GameState.EndedDraw
-                          || state==GameState.EndedWhiteWon)
+                    if ((state == GameState.EndedBlackWon
+                          || state == GameState.EndedDraw
+                          || state == GameState.EndedWhiteWon)
                         && !written
-                        && cnt>5){
+                        && cnt > 5)
+                    {
                         written = true;
-                        var filename = "5dpgn"+DateTime.Now.ToString("yyyyMMdd_HHmmss")+".txt";
-                        File.WriteAllText(filename,lastRun);
+                        var filename = "5dpgn" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".txt";
+                        File.WriteAllText(filename, lastRun);
                         Console.WriteLine($"written to file '{filename}'");
                     }
                 }
@@ -206,9 +279,11 @@ namespace FiveDChessRecorderCrossplatform
                 }
             }
         }
-        
-        private static string GameStateToResult(GameState s){
-            return s switch {
+
+        private static string GameStateToResult(GameState s)
+        {
+            return s switch
+            {
                 GameState.NotStarted => "NotStarted",
                 GameState.Running => "*",
                 GameState.EndedDraw => "1/2-1/2",
@@ -217,7 +292,7 @@ namespace FiveDChessRecorderCrossplatform
                 _ => "error"
             };
         }
-        
+
         internal static void WriteConsoleColored(string text, ConsoleColor foreground, ConsoleColor background)
         {
             var fOld = Console.ForegroundColor;
